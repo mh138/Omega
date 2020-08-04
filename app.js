5f93353a9c5e23a8d95f58c6c14760a78ef44443
mh138 <69214851+mh138@users.noreply.github.com>
2020-08-05 00:27:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDy95_mcB7_HIeYXGQ0WsPRJMSmTwHXBA0",
+  authDomain: "ig-hack-44bf4.firebaseapp.com",
+  databaseURL: "https://ig-hack-44bf4.firebaseio.com",
+  projectId: "ig-hack-44bf4",
+  storageBucket: "ig-hack-44bf4.appspot.com",
+  messagingSenderId: "369341947995",
+  appId: "1:369341947995:web:00b9e9f1182cbe75145f7e",
+  measurementId: "G-MKZB6J8PFZ"
+};
 };
 
      
